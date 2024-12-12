3061fde8ba08eb754d26dfc46fb2a6fa6c665f66
valdetad <valdeta.demiri@hotmail.com>
2024-12-12 15:46:57 +0100

    using lombok instead of constructors

--- StockManagement/src/main/java/com/example/StockManagement/data/model/webSocket/ChatMessage.java
+++ StockManagement/src/main/java/com/example/StockManagement/data/model/webSocket/ChatMessage.java
@@ -1,5 +1,10 @@
 package com.example.StockManagement.data.model.webSocket;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ChatMessage {
     private String content;
     private String sender;
@@ -9,27 +14,4 @@ public class ChatMessage {
         CHAT, LEAVE, JOIN
     }
 
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public MessageType getType() {
-        return type;
-    }
-
-    public void setType(MessageType type) {
-        this.type = type;
-    }
 }
