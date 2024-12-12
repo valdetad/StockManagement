221fc399bc804ed5a260b2c15aca511516618bcf
valdetad <valdeta.demiri@hotmail.com>
2024-12-12 16:06:29 +0100

    refactoring code

--- StockManagement/src/main/java/com/example/StockManagement/repository/MarketRepository.java
+++ StockManagement/src/main/java/com/example/StockManagement/repository/MarketRepository.java
@@ -2,6 +2,8 @@ package com.example.StockManagement.repository;
 
 import com.example.StockManagement.data.model.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MarketRepository extends JpaRepository<Market, Long> {
 }
