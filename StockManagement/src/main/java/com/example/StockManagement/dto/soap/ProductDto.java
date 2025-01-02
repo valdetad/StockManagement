ec5e1d7d2f5d7ca4fa2cba2758127550188dee99
valdetad <valdeta.demiri@hotmail.com>
2025-01-02 15:45:08 +0100

    Added endpoint for search product

---
+++ StockManagement/src/main/java/com/example/StockManagement/dto/soap/ProductDto.java
@@ -0,0 +1,21 @@
+package com.example.StockManagement.dto.soap;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Product", namespace = "https://example.com/stock", propOrder = {"id", "name", "description", "price", "quantity"})
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+}
