df43340624485427ae77a503445f91b24d3e928e
valdetad <valdeta.demiri@hotmail.com>
2025-01-07 00:02:07 +0100

    made small fixes in Product Dto

--- StockManagement/src/main/java/com/example/StockManagement/data/model/Product.java
+++ StockManagement/src/main/java/com/example/StockManagement/data/model/Product.java
@@ -1,15 +1,20 @@
 package com.example.StockManagement.data.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name="product")
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "name")
