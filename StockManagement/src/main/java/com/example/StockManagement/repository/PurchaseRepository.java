641d404a4faf4d0aaab4b7290759b429d0f63078
valdetad <valdeta.demiri@hotmail.com>
2025-01-08 21:39:38 +0100

    added annotation for repository

--- StockManagement/src/main/java/com/example/StockManagement/repository/PurchaseRepository.java
+++ StockManagement/src/main/java/com/example/StockManagement/repository/PurchaseRepository.java
@@ -2,6 +2,8 @@ package com.example.StockManagement.repository;
 
 import com.example.StockManagement.data.model.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
 }
