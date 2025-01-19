01c9fe8d625eeefa76c1ad717dca609127e2d3a3
valdetad <valdeta.demiri@hotmail.com>
2025-01-19 22:01:45 +0100

    Made changes and removed not neccessary classes

--- StockManagement/src/main/java/com/example/StockManagement/data/model/security/model/EHistoryType.java
+++
@@ -1,9 +0,0 @@
-//package com.example.StockManagement.data.model.security.model;
-//
-//public enum EHistoryType {
-//    PRIMARY_SCHOOL,
-//    HIGH_SCHOOL,
-//    BACHELOR_SCHOOL,
-//    EMPLOYMENT_STATUS
-//}
-//
